0155e89b0eaa839eddf8b3a5d5b053a2bbf18079
Mattias Benninge <crazymatt@hotmail.com>
2023-05-31 12:58:57 +0200

    New-BranchCacheEnabledWinPE v1.0.0.3

--- New-BranchCacheEnabledWinPE.ps1
+++ New-BranchCacheEnabledWinPE.ps1
@@ -18,47 +18,52 @@
     1.0.0.0 : 11/26/2019  : Initial version of script 
     1.0.0.1 : 11/26/2019  : Added version checks for WinPEGen.exe and StifleR Client
     1.0.0.2 : 10/21/2022  : Added Backup and Certificate Import
+    1.0.0.3 : 05/31/2023  : Version check fix
 
    .LINK
     https://2pintsoftware.com
 #>
-
 # Requires the script to be run under an administrative account context.
 #Requires -RunAsAdministrator
 
-#
-# Parameters region BEGIN
-#
+#region --------------------------------------------------[Script Parameters]------------------------------------------------------
 
 # Set OS Image to get BranchCache binaries from. The OS version (build number, e.g. patch-level) must match boot image version.
 # If using a newer Windows 11 image, patch the boot media to same level
-$Windows11Media = "E:\Setup\Windows 11 21H2 WIM\install.wim"
+$Windows11Media = "E:\Sources\OSD\OS\Windows 11 Enterprise x64 v21H2\sources\install.wim"
 
 # Get the StifleR Client files from a full Windows StifleR client install (copy entire folder)
-$StifleRSource = "E:\Setup\StifleR Client - Installed - 2.6.9.0"
+$StifleRSource = "E:\Source\OSD\2Pint\Stifler Client"
 
 # Get the StifleR Client config file from a full Windows client 
 $StifleRClientRules = "$StifleRSource\StifleR.ClientApp.exe.Config"
+
+# Path to WinPEGen.exe
+$OSDToolkitPath = "E:\Source\OSD\2Pint\OSD Toolkit 3.0.9.2 - Full\x64"
 
 # List indexes in WIM Image
 # Get-WindowsImage -ImagePath $Windows11Media
 
 # Set other parameters
-$BackupBootMedia = "E:\Sources\OSD\Boot\Zero Touch WinPE 10 x64 - OSD Toolkit and WiFi\Backup\winpe.wim" # Optional, only needed when running the script multiple times
-$BootMedia = "E:\Sources\OSD\Boot\Zero Touch WinPE 10 x64 - OSD Toolkit and WiFi\WinPE.wim"
+$BackupBootMedia = "E:\Source\OSD\Boot\2Pint_Win11_21H2\Backup\winpe.wim" # Optional, only needed when running the script multiple times
+$BootMedia = "E:\Source\OSD\Boot\2Pint_Win11_21H2\winpe.wim"
 $BootIndex = "1"
-$Windows11Index = "3" #  Index 3 is Enterprise if using the WIM from a Microsoft ISO
-$OSDToolkitPath = "E:\Setup\2Pint Software OSD Toolkit 3.0.2.0\x64"
 
-#
-# Parameters region END
-#
+$Windows11Index = "3" #  Index 3 is Enterprise if using the WIM from a Microsoft ISO
+
+# Optional Parameters
+# Add the Root CA if using internal PKI for StifleR (certutil -ca.cert C:\Setup\Cert\CustomerRootCA.cer)
+# Comment below if not used (add # infront)
+$Cert = "E:\Setup\Cert\CustomerRootCA.cer"
+$MountPath = "E:\Mount" # Used to mount bootimage before injecting certificat, required if $Cert is used.
+
+#endregion -----------------------------------------------[Script Parameters]------------------------------------------------------
 
 # Validation
-$WinPEGenVersion=(Get-ItemProperty "$OSDToolkitPath\WinPEGen.exe").VersionInfo.FileVersion
-$StifleRClientVersion=(Get-ItemProperty "$StifleRSource\StifleR.ClientApp.exe").VersionInfo.FileVersion
-If ($WinPEGenVersion -lt "3.0.2.0"){Write-Warning "WinPEGen version too old. Aborting script...";Break}
-If ($StifleRClientVersion -lt "2.6.9.0"){Write-Warning "StifleR Client version too old. Aborting script...";Break}
+$WinPEGenVersion=[version](Get-ItemProperty "$OSDToolkitPath\WinPEGen.exe").VersionInfo.FileVersion
+$StifleRClientVersion=[version](Get-ItemProperty "$StifleRSource\StifleR.ClientApp.exe").VersionInfo.FileVersion
+If ($WinPEGenVersion -lt [version]"3.0.2.0"){Write-Warning "WinPEGen version too old. Aborting script...";Break}
+If ($StifleRClientVersion -lt [version]"2.6.9.0"){Write-Warning "StifleR Client version too old. Aborting script...";Break}
 
 If (!(Test-Path $Windows11Media)){Write-Warning "$Windows11Media missing, aborting script...";Break}
 If (!(Test-Path $StifleRSource)){Write-Warning "$StifleRSource missing, aborting script...";Break}
@@ -79,12 +84,11 @@ Set-Location $OSDToolkitPath
 # Delete the WinPEGen Backup
 Remove-Item -Path "$($BootMedia)_original_backup" -Force
 
-# Add the Root CA if using internal PKI for StifleR (certutil -ca.cert C:\Setup\Cert\ViaMonstraRootCA.cer)
-$Cert = "E:\Setup\Cert\ViaMonstraRootCA.cer"
-$MountPath = "E:\Mount"
-Mount-WindowsImage -ImagePath $BootMedia -Index 1 -Path $MountPath
-Copy-Item -Path $Cert -Destination "$MountPath\Windows\System32" -Force 
-Dismount-WindowsImage -Path $MountPath -Save
+if($Cert) {
+    Mount-WindowsImage -ImagePath $BootMedia -Index 1 -Path $MountPath
+    Copy-Item -Path $Cert -Destination "$MountPath\Windows\System32" -Force 
+    Dismount-WindowsImage -Path $MountPath -Save
+}
 
 # Friendly reminder
 Write-Host ""
